ce65db48802fb141d63ed981340ab031983b79c5
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-02 23:47:07 -0800

    Day 3 Cleanup

--- 3/3-a.js
+++ 3/3-a.js
@@ -1,35 +1,18 @@
+//@ts-check
 const readFile = require("fs").readFileSync;
-const file = readFile("input", "utf-8").split("\n");
+const file = readFile("input", "utf-8").split("\n").map(_ => _.trim());
 
-const readings = file.map( _ => _.trim());
+const bitsets = file.map(_ => parseInt(_, 2));
+const BITLEN = file[0].length;
 
-const bitlen = readings[0].length;
-const bitsets = readings.map(_ => parseInt(_, 2));
+let gamma = 0, epsilon  = 0;
 
-const buckets = []
-
-const testBit = (num, pos) => (num & 1 << pos) != 0;
-const setBit = (num, pos) => num | (1 << pos);
-
-bitsets.forEach(bits => {
-    for (let i = 0; i < bitlen; i++) {
-        if (!buckets[i]) buckets[i] = 0;
-        buckets[i] += testBit(bits, i);
-    }
-});
-
-let first = 0;
-let second = 0;
-
-for (bit in buckets) {
-    if (buckets[bit] > readings.length/2) {
-        first = setBit(first, bit)
-    }
-    else {
-        second = setBit(second, bit)
-    }
+for (let i = BITLEN - 1; i >= 0; i--) {
+    const isSet = bitsets.filter(bits => bits >> i & 1).length < bitsets.length/2 ? 0 : 1;
+    gamma |= (isSet << i);
+    epsilon |= (1 - isSet << i);
 }
 
-
-console.log(first, second); //2663 1432
-console.log(first * second); //3813416
+console.log(`Gamma Rate:\t\t${gamma} - ${gamma == 2663 ? "✔" : "✖"}`);
+console.log(`Epsilon Rate:\t\t${epsilon} - ${epsilon == 1432 ? "✔" : "✖"}`);
+console.log(`Power Consumption:\t${gamma * epsilon} - ${gamma * epsilon == 3813416 ? "✔" : "✖"}`);
--- 3/3-b.js
+++ 3/3-b.js
@@ -1,51 +1,31 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
-const file = readFile("input", "utf-8").split("\n").map( _ => _.trim());
+const file = readFile("input", "utf-8").split("\n").map(_ => _.trim());
 
 const bitsets = file.map(_ => parseInt(_, 2));
+const BITLEN = file[0].length;
 
-const testBit = (num, pos) => (num & 1 << pos) != 0;
-const getBit = (num, pos) => (num & (1 << pos)) === 0 ? 0 : 1;
-/**
- * 
- * @param {number} num1 
- * @param {number} num2 
- * @param {number} k 
- * @returns 
- */
-const compareKthBit = (num1, num2, k) => !testBit(num1 ^ num2, k);
+let data = [bitsets, bitsets];  //Two copies of array to be able to filter for both highest and lowest at same time
 
-/**
- * @param {number[]} arr
- * @param {number} k
- */
-function isMostCommonBitSet(arr, k) {
-    return arr.filter(bits => testBit(bits, k)).length < arr.length/2
-}
-
-
-const bitlen = file[0].length;
-let o2, co2;
-
-let copy = bitsets;
-for (let i = bitlen - 1; i >= 0; i--) {
-    const isSet = isMostCommonBitSet(copy, i) ? 1 : 0;
-    copy = copy.filter((v) => compareKthBit(v, isSet << i, i));
-    if (copy.length == 1) {
-        o2 = copy[0];
-        break;
+for (let i = BITLEN - 1; i >= 0; i--) {
+    if (data[0].length > 1) {
+        /**
+         * 1. Filter the array (don't save the filtered array) to only have numbers who's ith bit is set
+         * 2. If the length of the filtered array is greater than half of that of the orginal array then the most common state in this position is set (else contrapositive)
+         * 3. Filter the array (and save the filtered array) to only have numbers whose ith bit is in the same state as most of those the original array 
+         */
+        const isSet = data[0].filter(bits => bits >> i & 1).length < data[0].length/2 ? 0 : 1;
+        data[0] = data[0].filter((v) => (v >> i & 1) == isSet);
+    }
+    if (data[1].length > 1) {
+        // The same as above but the result of the test for the most common state is reversed 
+        const isSet = data[1].filter(bits => bits >> i & 1).length < data[1].length/2 ? 1 : 0;
+        data[1] = data[1].filter((v) => (v >> i & 1) == isSet);
     }
 }
 
-copy = bitsets;
-for (let i = bitlen - 1; i >= 0; i--) {
-    const isSet = isMostCommonBitSet(copy, i) ? 0 : 1;
-    copy = copy.filter((v) => compareKthBit(v, isSet << i, i));
-    if (copy.length == 1) {
-        co2 = copy[0];
-        break;
-    }
-}
+const [[o2], [co2]] = data;
 
-console.log(o2, co2); //1184 2526
-console.log(o2 * co2); //2990784
+console.log(`O2 Generator Rating:\t${o2} - ${o2 == 2526 ? "✔" : "✖"}`);
+console.log(`CO2 Scrubber Rating:\t${co2} - ${co2 == 1184 ? "✔" : "✖"}`);
+console.log(`Life support rating:\t${o2 * co2} - ${o2 * co2 == 2990784 ? "✔" : "✖"}`);
