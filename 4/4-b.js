2621569514c71cfeec5d5fce7ef1d09d75cdf527
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-05 15:37:44 -0800

    Day 4 Cleanup

--- 4/4-b.js
+++ 4/4-b.js
@@ -4,86 +4,34 @@ const file = readFile("input", "utf-8").split("\n").map(_ => _.trim());
 
 const data = file.filter(_ => _ != "");
 const draws = data.shift().split(",").map(_ => parseInt(_));
+const boardData = data.map(_ => _.split(new RegExp(" +")).map(_ => parseInt(_)));
 let boards = [];
 
-let boardCount = 0;
-while (data.length > 0) {
+while (boardData.length > 0) {
     let board = [];
-    for (let i = 0; i < 5; i++) {
-        board.push(data.shift().split(new RegExp(" +")).map(_ => parseInt(_)));
-    }
+    for (let i = 0; i < 5; i++) board.push(boardData.shift());
     boards.push(board);
-    boardCount++;
 }
 
-
 /**
- * 
  * @param {number[][]} board 
  * @param {number} num
  */
-function toggleNumber(board, num) {
-    return board.map(_ => _.map(_ => _ = _ == num ? null : _));
-}
+const toggleNumber = (board, num) => board.map(_ => _.map(_ => _ = _ == num ? null : _));
 
-/**
- * 
- * @param {number[][]} board 
- */
+/**@param {number[][]} board */
 function checkBoard(board) {
-    //Check horizontal
-    let temp = board.map(_ => _.filter(_ => _ !== null).length);
-    for (const row of temp) if (row == 0) return true;
-
-    //Veritcal
-    let temp2 = [[],[],[],[],[],]
-    for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < 5; j++) {
-            temp2[j][i] = board[i][j]
-        }
-    }
-    let temp3 = temp2.map(_ => _.filter(_ => _ !== null).length);
-    for (const row of temp3) if (row == 0) return true;
-
-    //Diagonal, bruh I wasted time by 1 - getting this to word and 2 - not realizing for the longest time that this was the source of the inacurate result
-    // let temp4 = [[],[]]
-    // for (let i = 0; i < 5; i++) {
-    //     temp4[0].push(board[i][i]);
-    //     temp4[1].push(board[4 - i][i]);
-    // }
-    // let temp5 = temp4.map(_ => _.filter(_ => _ !== null).length);
-    // for (const row of temp5) if (row == 0) return true;
-
-    return false
+    const horizontalWins = board.some(_ => _.every(_ => _ == null));
+    const verticalWins = board.map((_,i) => board.map(_ => _[i])).some(_ => _.every(_ => _ == null));
+    return horizontalWins || verticalWins;
 }
 
-let lastdraw;
-let done = false;
+let lastdraw = 0;
 for (const draw of draws) {
-    if (done) break;
-
-    for (const i in boards) {
-        boards[i] = toggleNumber(boards[i], draw);
-    }
-
-    
-
-    for (const j in boards) {
-        if (checkBoard(boards[j])) {
-            if (boards.length > 1) boards.splice(parseInt(j), 1);
-            else {
-                lastdraw= draw;
-                done = true;
-            }
-        }
-    }
-
+    boards = boards.map(_ => toggleNumber(_, draw));
+    if (boards.some((_, i) => checkBoard(_) && (boards.length > 1 ? boards.splice(i, 1) && false : lastdraw = draw))) break;
 }
 
-let sum = boards[0].reduce((acc, cur) => {
-    console.log(cur);
-    return acc += cur.reduce((acc, cur) => acc += cur ?? 0)
-}, 0);
+const sum = boards[0].reduce((acc, cur) => acc += cur.reduce((acc, cur) => acc += cur ?? 0), 0);
 
-console.log(sum,  lastdraw);
 console.log(sum * lastdraw);
