c0b0b5862355c77deeba837bba5e2e6d690a1aaa
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-05 21:37:33 -0800

    Day 6 Cleanup

--- 6/6-a.js
+++ 6/6-a.js
@@ -1,25 +1,14 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
-const file = readFile("input", "utf-8").split("\n").map(_ => _.trim().split(",").map(_ => parseInt(_))).flat();
-
-let fish = file;
+let fish = readFile("input", "utf-8").split(",").map(_ => parseInt(_));
 
 const days = 80;
 const cycle = 6;
 
 for (let i = 0; i < days; i++) {
     let newFish = [];
-    fish.forEach((v, i, arr) => {
-        if (v == 0) {
-            arr[i] = cycle;
-            newFish.push(cycle + 2);
-        } else {
-            arr[i] = v - 1;
-        }
-    });
+    fish = fish.map((v) => v == 0 ? (newFish.push(cycle + 2) * 0) + cycle : v - 1);
     fish.push(...newFish);
 }
 
-const sum = fish.length;
-
-console.log(sum);
+console.log(fish.length);
--- 6/6-b.js
+++ 6/6-b.js
@@ -1,27 +1,17 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
-const file = readFile("input", "utf-8").split("\n").map(_ => _.trim().split(",").map(_ => parseInt(_))).flat();
-
-let fish = file;
+const initial = readFile("input", "utf-8").split(",").map(_ => parseInt(_));
 
 const days = 256;
 const cycle = 6;
+const fishes = Array(cycle + 3).fill(0);
 
-const fishes = Array(9).fill(0);
-
-fish.forEach(_ => fishes[_]++);
+initial.forEach(_ => fishes[_]++);
 
 for (let i = 0; i < days; i++) {
-    const fishCopy = [...fishes];
-    fishes[0] = fishCopy[1];
-    fishes[1] = fishCopy[2];
-    fishes[2] = fishCopy[3];
-    fishes[3] = fishCopy[4];
-    fishes[4] = fishCopy[5];
-    fishes[5] = fishCopy[6];
-    fishes[6] = fishCopy[7] + fishCopy[0];
-    fishes[7] = fishCopy[8];
-    fishes[8] = fishCopy[0];
+    const newFish = fishes[0];
+    fishes.push(fishes.shift());
+    fishes[cycle] += newFish;
 }
 
 const sum = fishes.reduce((acc, cur) => acc += cur);
