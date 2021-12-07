3bebd0fa40031bbe3365d7bf7b6329a75450880a
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-06 21:55:42 -0800

    Day 7 Cleanup

--- 7/7-a.js
+++ 7/7-a.js
@@ -1,14 +1,8 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
 const file = readFile("input", "utf-8").split(",").map(_ => parseInt(_));
+let crabs = Array(file.length).fill(0);
 
-const costs = [];
+crabs = crabs.map((v, i) => file.reduce((acc, cur) => acc += Math.abs(cur - i), 0));
 
-file.forEach((v, i, arr) => {
-    costs.push(arr.reduce((acc, cur) => acc += Math.abs(cur - i)));
-})
-
-// I don't know why I'm off by the index, but whatever
-const i = costs.findIndex(v => v == Math.min(...costs));
-
-console.log(Math.min(...costs) - i);
+console.log(Math.min(...crabs));
--- 7/7-b.js
+++ 7/7-b.js
@@ -1,13 +1,9 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
 const file = readFile("input", "utf-8").split(",").map(_ => parseInt(_));
+const sum = (n) => (n * (n + 1))/2;
+let crabs = Array(file.length).fill(0);
 
-let costs = [];
-const sum = (n) => (n * (n + 1))/2
+crabs = crabs.map((v, i) => file.reduce((acc, cur) => acc += sum(Math.abs(cur - i)), 0));
 
-file.forEach((v, i, arr) => {
-    // This doesn't work without the 0 as an initial value, this caused me to lose like half an hour lmao
-    costs.push(arr.reduce((acc, cur) => acc += sum(Math.abs(cur - i)), 0));
-})
-
-console.log(Math.min(...costs));
+console.log(Math.min(...crabs));
