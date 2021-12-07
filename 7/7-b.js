49c926161d13cae0902757d71d3a9584210dd18f
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-06 21:56:35 -0800

    Note for future me >:(

--- 7/7-b.js
+++ 7/7-b.js
@@ -4,6 +4,7 @@ const file = readFile("input", "utf-8").split(",").map(_ => parseInt(_));
 const sum = (n) => (n * (n + 1))/2;
 let crabs = Array(file.length).fill(0);
 
+// This doesn't work without the 0 as an initial value for the inner reduce, this caused me to lose like half an hour xd
 crabs = crabs.map((v, i) => file.reduce((acc, cur) => acc += sum(Math.abs(cur - i)), 0));
 
 console.log(Math.min(...crabs));
