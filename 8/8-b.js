0acefe9bc6b2500eda87b779ed14845b1f22c3e9
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-07 23:52:17 -0800

    Day 8 Cleanup

--- 8/8-b.js
+++ 8/8-b.js
@@ -1,82 +1,34 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
 const file = readFile("input", "utf-8").split("\n").map(_ => _.trim());
-const displays = [];
 
-class Display {
-    /**@type {string[]} */
-    unknowns;
-    /**@type {string[]} */
-    output;
-    /**@type {string[]} */
-    knowns;
+// [[unknowns, output, knowns]]
+const displays = file.map(line => {
+    const data = line.split(" | ").map(list => {
+        return list.split(" ").map(segments => segments.split("").sort().join(""));
+    });
+    data.push(Array(10).fill(""));
+    return data;
+});
 
-
-    /**@param {string} data */
-    constructor(data) {
-        const split = data.split("|").map(_ => _.trim());
-        this.unknowns = split[0].split(" ").map(_ => _.split("").sort().join(""));
-        this.output = split[1].split(" ").map(_ => _.split("").sort().join(""));
-        this.knowns = Array(10).fill(null);
-        console.log(this.unknowns, this.output, this.knowns);
-        this.solve();
-        console.log(this.unknowns, this.output, this.knowns);
-        console.log("===========================");
-    }
-
-    solve() {
-        // 1
-        for(const str of this.unknowns) if (str.length == 2) {this.makeKnown(str, 1); console.log(1);}
-        // 7
-        for(const str of this.unknowns) if (str.length == 3) {this.makeKnown(str, 7); console.log(7);}
-        // 4
-        for(const str of this.unknowns) if (str.length == 4) {this.makeKnown(str, 4); console.log(4);}
-        // 8
-        for(const str of this.unknowns) if (str.length == 7) {this.makeKnown(str, 8); console.log(8);}
-        // 3
-        for(const str of this.unknowns) if (str.length == 5 && this.hasAllOf(this.knowns[1], str)) {this.makeKnown(str, 3); console.log(3);}
-        // 9
-        for(const str of this.unknowns) if (this.hasAllOf(this.knowns[4], str)) {this.makeKnown(str, 9); console.log(9);}
-        // 0
-        for(const str of this.unknowns) if (str.length == 6 && this.hasAllOf(this.knowns[7], str)) {this.makeKnown(str, 0); console.log(0);}
-        // 5
-        for(const str of this.unknowns) if (str.length == 5 && this.hasAllOf(str, this.knowns[9])) {this.makeKnown(str, 5); console.log(5);}
-        // 6
-        for(const str of this.unknowns) if (str.length == 6) {this.makeKnown(str, 6); console.log(6);}
-
-        // 2
-        // for(const str of this.unknowns) if (str.length == 5) {this.makeKnown(str, 2); console.log(2);}
-        
-        this.makeKnown(this.unknowns[0], 2);
-        console.log(2);
-    }
-
-    /**
-     * @param {string} str 
-     * @param {number} num 
-     */
-    makeKnown(str, num) {
-        if (this.knowns[num]) {
-            console.log(`${str} is overwriting ${this.knowns[num]}`);
-            console.log(this.unknowns, this.output, this.knowns);
-        }
-        this.knowns[num] = str;
-        this.unknowns.splice(this.unknowns.indexOf(str), 1);
-    }
-
-    /**
-     * @param {string} mask 
-     * @param {string} test 
-     */
-    hasAllOf(mask, test) {
-        console.log(mask, test);
-        return mask.split("").every(char => test.includes(char));
-    }
-
-    /**@returns {number}*/
-    value() {
-        return this.output.reduce((acc, v, i) => acc += this.knowns.indexOf(v) * Math.pow(10, this.output.length - 1 - i), 0);
-    }
+const isSubsetOf = (mask, test) => mask.split("").every(char => test.includes(char));
+const makeKnown = (data, str, num) => {
+    data[2][num] = str;
+    data[0].splice(data[0].indexOf(str), 1);
 }
 
-console.log(file.reduce((acc, data) => acc += new Display(data).value(), 0));
+const getValue = (data) => {
+    /*1*/for(const str of data[0]) if (str.length == 2) makeKnown(data, str, 1);
+    /*7*/for(const str of data[0]) if (str.length == 3) makeKnown(data, str, 7);
+    /*4*/for(const str of data[0]) if (str.length == 4) makeKnown(data, str, 4);
+    /*8*/for(const str of data[0]) if (str.length == 7) makeKnown(data, str, 8);
+    /*3*/for(const str of data[0]) if (str.length == 5 && isSubsetOf(data[2][1], str)) makeKnown(data, str, 3);
+    /*9*/for(const str of data[0]) if (isSubsetOf(data[2][4], str)) makeKnown(data, str, 9);
+    /*0*/for(const str of data[0]) if (str.length == 6 && isSubsetOf(data[2][7], str)) makeKnown(data, str, 0);
+    /*5*/for(const str of data[0]) if (str.length == 5 && isSubsetOf(str, data[2][9])) makeKnown(data, str, 5);
+    /*6*/for(const str of data[0]) if (str.length == 6) makeKnown(data, str, 6);
+    /*2*/makeKnown(data, data[0][0], 2);
+    return data[1].reduce((acc, v, i) => acc += data[2].indexOf(v) * Math.pow(10, data[1].length - 1 - i), 0)
+}
+
+console.log(displays.reduce((acc, data) => acc += getValue(data), 0));
