40ad698e943a627001843dd19fb45dab0b5b1e0d
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-09 22:25:56 -0800

    Fixing folder order

--- 06/6-b.js
+++ 06/6-b.js
@@ -2,16 +2,15 @@
 const readFile = require("fs").readFileSync;
 const initial = readFile("input", "utf-8").split(",").map(_ => parseInt(_));
 
-const days = 256;
+let days = 256;
 const cycle = 6;
 const fishes = Array(cycle + 3).fill(0);
 
 initial.forEach(_ => fishes[_]++);
 
-for (let i = 0; i < days; i++) {
-    const newFish = fishes[0];
+while(days--) {
+    fishes[cycle + 1] += fishes[0];
     fishes.push(fishes.shift());    //Cycles array elements
-    fishes[cycle] += newFish;
 }
 
 const sum = fishes.reduce((acc, cur) => acc += cur);
