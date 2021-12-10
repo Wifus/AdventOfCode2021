79804d129441d22b7c9845669b73c3f6048cdbcc
Wifu <62569962+Wifus@users.noreply.github.com>
2021-12-09 22:40:25 -0800

    Day 10 Removed Redundancy

--- 10/10-a.js
+++ 10/10-a.js
@@ -1,27 +1,19 @@
 //@ts-check
 const readFile = require("fs").readFileSync;
 const file = readFile("input", "utf-8").split("\n").map(_ => _.trim());
-const pairs = {
-    "(": ")",
-    "{": "}" ,
-    "[": "]",
-    "<": ">"
-}, points = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137
-}, invalid = [];
+const pairs = {"(": ")", "{": "}" , "[": "]", "<": ">"}, 
+      points = {")": 3, "]": 57, "}": 1197, ">": 25137}
+let score = 0;
 
 /**@param {string} line*/
-function getInvalidCharacter(line) {
-    let stack = [];
-    for(let char of line) {
+function addScore(line) {
+    const stack = [];
+    for(const char of line) {
         if (pairs[char]) stack.push(char);
-        else if (char != pairs[stack.pop()]) return char;
+        else if (char != pairs[stack.pop()]) {score += points[char]; break;}
     }
 }
 
-file.forEach(_ => invalid.push(getInvalidCharacter(_)));
+file.forEach(_ => addScore(_));
 
-console.log(invalid.reduce((acc, cur) => acc += points[cur] ?? 0, 0));
+console.log(score);
